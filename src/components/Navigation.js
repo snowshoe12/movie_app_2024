0184388609b48d26eb8ce55dec2455723cfcb87f
snowshoe12 <joungbeena@gmail.com>
2024-10-23 15:55:56 +0900

    Re

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,28 +18,7 @@ function Navigation() {
         </li>
       </ul>
 
-      {/* Search form placed outside the ul to float to the right */}
-      <div className="nav-search">
-        <form>
-          <input type="text" placeholder="Search..." />
-          <button type="submit">
-            {/* Magnifying glass icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-search"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <line x1="21" y1="21" x2="16.65" y2="16.65" />
-            </svg>
-          </button>
-        </form>
-      </div>
+      
     </div>
   );
 }
